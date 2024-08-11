e20742836ac8a606ff673ca873264a2f8c733786
Pradyumn Chaudhary <pradyumnchaudhary6@gmail.com>
2024-08-11 13:33:48 +0530

    Python Program

---
+++ Variable.py
@@ -0,0 +1,14 @@
+name = "Pradyumn Chaudhary" 
+superhero = 'IronMan'
+superheroName = '''Tony Stark'''
+age = 23
+cost = 45.98
+
+print(name)
+print(superhero)
+print(superheroName)
+print("age = " ,age, "cost = ",cost,".")
+
+print(type(name))
+print(type(age))
+print(type(cost))
